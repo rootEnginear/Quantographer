4dc1de48fee756f122afed601e86c7f2eee18cde
rootEnginear <suthep.chanchuphol@outlook.com>
2022-01-21 07:36:46 +0700

    feat: add shortcut handling template

--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,106 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import Menubar from './components/Menubar.vue'
 
 createApp(Menubar).mount('#menubar')
 
 import './styles/main.scss'
 
-import { gateButtons, canvasElement } from './element'
-import { drawCanvas, clearCanvas, updateSize } from './render'
+import {gateButtons, canvasElement} from './element'
+import {drawCanvas, clearCanvas, updateSize} from './render'
 
 import './ui/gate_tooltip'
 
 window.addEventListener('resize', () => {
-	updateSize()
-	clearCanvas()
-	drawCanvas()
+  updateSize()
+  clearCanvas()
+  drawCanvas()
 })
 
 document.fonts.ready.then(() => {
-	updateSize()
-	clearCanvas()
-	drawCanvas()
+  updateSize()
+  clearCanvas()
+  drawCanvas()
 })
 
 for (const gateButton of gateButtons)
-	gateButton.addEventListener('dragstart', (e) => {
-		const { target, dataTransfer } = e
+  gateButton.addEventListener('dragstart', (e) => {
+    const {target, dataTransfer} = e
 
-		const node = target as Node
-		const transfer = dataTransfer as DataTransfer
+    const node = target as Node
+    const transfer = dataTransfer as DataTransfer
 
-		const data = node.textContent ?? '?'
+    const data = node.textContent ?? '?'
 
-		transfer.setData('text/plain', data)
+    transfer.setData('text/plain', data)
 
-		console.log('dragstart', data)
-	})
+    console.log('dragstart', data)
+  })
 
 canvasElement.addEventListener('dragover', (e) => {
-	e.preventDefault()
+  e.preventDefault()
 
-	console.log('dragover', e.offsetX, e.offsetY)
+  console.log('dragover', e.offsetX, e.offsetY)
 })
 
 canvasElement.addEventListener('drop', (e) => {
-	e.preventDefault()
+  e.preventDefault()
 
-	const { dataTransfer } = e
-	const transfer = dataTransfer as DataTransfer
-	const data = transfer.getData('text/plain')
+  const {dataTransfer} = e
+  const transfer = dataTransfer as DataTransfer
+  const data = transfer.getData('text/plain')
 
-	console.log('drop', data)
+  console.log('drop', data)
 })
+
+// -----------------------------------------------------------------------------
+// Shortcuts
+// -----------------------------------------------------------------------------
+// Chrome: Ctrl+Shift+N, Ctrl+T, Ctrl+Shift+T, Ctrl+W, Ctrl+Shift+W don't work
+// https://stackoverflow.com/questions/7295508/javascript-capture-browser-shortcuts-ctrlt-n-w/7296303#7296303
+const handlingShortcuts = (e: KeyboardEvent) => {
+  if (e.ctrlKey && e.key === 'p')
+    togglePalette()
+
+
+  if (e.ctrlKey && e.key === 'b')
+    toggleCode()
+
+
+  if (e.ctrlKey && e.key === 'e')
+    execute()
+
+
+  if (e.ctrlKey && e.key === 'n')
+    location.reload()
+
+
+  e.stopPropagation()
+  e.preventDefault()
+  return 0
+}
+
+window.addEventListener('keydown', handlingShortcuts)
 
 // -----------------------------------------------------------------------------
 // DOM
 // -----------------------------------------------------------------------------
-function togglePalette() {
-	document.getElementById('pallette')?.classList.toggle('open')
+const togglePalette = () => {
+  document.getElementById('pallette')?.classList.toggle('open')
 }
 
-function toggleCode() {
-	document.getElementById('code')?.classList.toggle('open')
+const toggleCode = () => {
+  document.getElementById('code')?.classList.toggle('open')
 }
 
 const execute = () => {
-	new window.WinBox({
-		class: ['no-full'],
-		title: 'Basic Window',
-	})
+  new window.WinBox({
+    class: ['no-full'],
+    title: 'Execute'
+  })
 }
 
-Object.assign(window, { execute, togglePalette, toggleCode })
+Object.assign(window, {
+  execute,
+  togglePalette,
+  toggleCode
+})
