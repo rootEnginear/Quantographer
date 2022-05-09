b7ac3f6cdcefd3b32f07e7158258b76dec4db53f
rootEnginear <suthep.chanchuphol@outlook.com>
2022-05-10 01:22:46 +0700

    feat: add real recommend system

--- src/components/Execute.vue
+++ src/components/Execute.vue
@@ -6,15 +6,11 @@
         <div class="control">
           <div class="select is-fullwidth">
             <select v-model="sys">
-              <option value="manila">ibmq_manila (5 qubits, 32 QV)</option>
-              <option value="bogota">ibmq_bogota (5 qubits, 32 QV)</option>
-              <option value="santiago">ibmq_santiago (5 qubits, 32 QV)</option>
-              <option value="quito">ibmq_quito (5 qubits, 16 QV)</option>
-              <option value="belem">ibmq_belem (5 qubits, 16 QV)</option>
-              <option value="lima">ibmq_lima (5 qubits, 8 QV)</option>
-              <option value="armonk" :disabled="qubit_count > 1">ibmq_armonk (1 qubit, 1
-                QV)</option>
-              <option value="qasm_simulator">ibmq_qasm_simulator</option>
+              <option v-for="s in all_sys" :key="s.name" :value="s.name"
+                :disabled="qubit_count > (s?.qb || 0) && s.name !== 'ibmq_qasm_simulator'">
+                {{ s.name }} ({{ s.qb || 0 }} qubit{{ (s.qb || 0) > 1 ? 's' : '' }}<span
+                  v-if="s.name !== 'ibmq_qasm_simulator'">, {{ s.qv || 0 }} QV</span>)
+              </option>
             </select>
           </div>
         </div>
@@ -108,8 +104,8 @@
       </div>
     </template>
     <template v-else-if="transpile_result_status === 'ERROR'">
-      <article class="message is-danger">
-        <div class="message-body">
+      <article class="message is-danger" style="height:100%">
+        <div class="message-body" style="height:100%">
           <h1>An error occured!</h1>
           <p>Error: {{ transpile_result }}</p>
         </div>
@@ -132,15 +128,15 @@
       </span>
     </button>
   </div>
-  <template v-if="backend_status !== 'IDLE'">
+  <template v-if="overlay_status !== 'IDLE'">
     <div class="execute-master-loader">
-      <template v-if="backend_status === 'RESULT'">
+      <template v-if="overlay_status === 'RESULT'">
         <div class="box">
           <h2>Recommended Configuration:</h2>
           <ul style="padding-left:2ch">
             <li>
               <strong style="margin:0;display:inline">IBMQ System:</strong>
-              &nbsp;<code>ibmq_{{ rec_sys }}</code>
+              &nbsp;<code>{{ rec_sys }}</code>
             </li>
             <li>
               <strong style="margin:0;display:inline">Optimization Level:</strong>
@@ -156,25 +152,48 @@
             </li>
             <li>
               <strong style="margin:0;display:inline">Schedule Method:</strong>
-              &nbsp;<code>{{ toTitleCase(rec_sdmt) }}</code>
+              &nbsp;<code>None</code>
             </li>
           </ul>
           <div class="row">
             <button class="button is-primary is-fullwidth" @click="useConfig()">
               <span>Use this configuration</span>
             </button>
-            <button class="button" @click="closeRecommendation()">
+            <button class="button" @click="closeOverlay()">
               <span>Discard</span>
             </button>
           </div>
         </div>
+      </template>
+      <template v-else-if="overlay_status === 'ERROR'">
+        <article class="message is-danger">
+          <div class="message-header">
+            <p>An error occured!</p>
+          </div>
+          <div class="message-body">
+            <p style="margin-bottom:1rem">
+              {{ overlay_status_text }}
+            </p>
+            <div class="row">
+              <button class="button is-fullwidth is-danger" @click="retry_function()">
+                <span class="icon is-small">
+                  <i class="fas fa-arrow-rotate-right"></i>
+                </span>
+                <span>Retry</span>
+              </button>
+              <button class="button" @click="closeOverlay()">
+                <span>Discard</span>
+              </button>
+            </div>
+          </div>
+        </article>
       </template>
       <template v-else>
         <div class="spinner" style="width:25%;margin:1rem 0">
           <div class="double-bounce1"></div>
           <div class="double-bounce2"></div>
         </div>
-        <h1 style="text-align:center">{{ backend_status_text }}</h1>
+        <h1 style="text-align:center">{{ overlay_status_text }}</h1>
       </template>
     </div>
   </template>
@@ -183,96 +202,139 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const backend_status = ref<'IDLE' | 'FETCHING' | 'ERROR' | 'RESULT'>('FETCHING')
-const backend_status_text = ref("Connecting to Backends...")
+const overlay_status = ref<'IDLE' | 'FETCHING' | 'ERROR' | 'RESULT'>('FETCHING')
+const overlay_status_text = ref("Connecting to Backends...")
 
 const transpile_result_status = ref<'IDLE' | 'FETCHING' | 'ERROR'>('FETCHING')
 const transpile_result = ref("")
 
-const sys = ref("manila")
+const all_sys = ref([
+  { name: 'ibmq_qasm_simulator', qb: 32, qv: null },
+])
+
+const sys = ref("ibmq_qasm_simulator")
 const otlv = ref("0")
 const rtmt = ref("basic")
 const lomt = ref("trivial")
 const sdmt = ref("none")
 
-const rec_sys = ref("manila")
+const rec_sys = ref("ibmq_qasm_simulator")
 const rec_otlv = ref("2")
 const rec_rtmt = ref("stochastic")
 const rec_lomt = ref("noise_adaptive")
-const rec_sdmt = ref("none")
 
 const qubit_count = ref(0)
 
-const simulateBackend = () => new Promise<void>((res) => {
-  setTimeout(() => {
-    res()
-  }, Math.floor(Math.random() * (2000 - 500 + 1)) + 500)
-})
+const retry_function = ref(() => {})
 
-const fetchAvailableBackends = () => {
-  backend_status.value = 'FETCHING'
-  backend_status_text.value = 'Connecting to Backends...'
-  simulateBackend().then(() => {
+const fetchAvailableBackends = async () => {
+  overlay_status.value = 'FETCHING'
+  overlay_status_text.value = 'Connecting to Backends...'
+
+  try {
+    const resp = await fetch('https://quantum-backend-flask.herokuapp.com/get_backend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // @ts-expect-error
+        api_key: window.getApiKey()
+      })
+    })
+    const r = await resp.json()
+    if ('error' in r) throw new Error(r.error)
+    const backends = r.sort((b: any, a: any) => a.qb === b.qb ? (a.qv === b.qv ? b.name.localeCompare(a.name) : a.qv - b.qv) : a.qb - b.qb)
+    all_sys.value = backends
+
+    const available_backend = backends.find((backend: any) => backend.qb >= qubit_count.value)
+    if (!available_backend) throw new Error("We get the backends but your circuit is too big for any backends.")
+    sys.value = available_backend.name
+
     // automatic retranspile
     updateTranspileResult()
 
-    backend_status.value = 'IDLE'
-  })
+    overlay_status.value = 'IDLE'
+
+  } catch (e: any) {
+    overlay_status.value = 'ERROR'
+    overlay_status_text.value = `Cannot get backends: ${e?.message || JSON.stringify(e)}`
+    retry_function.value = fetchAvailableBackends
+  }
 }
 
-const updateTranspileResult = () => {
+const updateTranspileResult = async () => {
   transpile_result_status.value = 'FETCHING'
-  fetch('https://quantum-backend-flask.herokuapp.com/transpile', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      // @ts-expect-error
-      code: window.translateCircuit(),
-      // TODO: Change this system to other systems
-      system: sys.value,
-      layout: lomt.value,
-      routing: rtmt.value,
-      scheduling: sdmt.value === 'none' ? null : sdmt.value,
-      optlvl: +otlv.value
+  try {
+    const resp = await fetch('https://quantum-backend-flask.herokuapp.com/transpile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // @ts-expect-error
+        code: window.translateCircuit(),
+        // @ts-expect-error
+        api_key: window.getApiKey(),
+        system: sys.value,
+        layout: lomt.value,
+        routing: rtmt.value,
+        scheduling: sdmt.value === 'none' ? null : sdmt.value,
+        optlvl: +otlv.value
+      })
     })
-  }).then((r) => r.json())
-    .then((r) => {
-      transpile_result.value = r.pic
-      transpile_result_status.value = 'IDLE'
-    }).catch(e => {
-      transpile_result.value = e?.message || JSON.stringify(e)
-      transpile_result_status.value = 'ERROR'
-    })
+    const r = await resp.json()
+    if ('error' in r) throw new Error(r.error)
+    transpile_result.value = r.pic
+    transpile_result_status.value = 'IDLE'
+  } catch (e: any) {
+    transpile_result.value = e?.message || JSON.stringify(e)
+    transpile_result_status.value = 'ERROR'
+  }
 }
 
 const getRecommendation = async () => {
-  backend_status.value = 'FETCHING'
-  backend_status_text.value = 'Getting Recommendation...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 1/6 systems...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 2/6 systems...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 3/6 systems...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 4/6 systems...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 5/6 systems...'
-  await simulateBackend()
-  backend_status_text.value = 'Brute forcing 6/6 systems...'
-  await simulateBackend()
-  simulateRecommend()
-  backend_status.value = 'RESULT'
-}
+  overlay_status.value = 'FETCHING'
+  overlay_status_text.value = 'Getting Recommendation...'
 
-const simulateRecommend = () => {
-  // random 0 - 3
-  rec_sys.value = ["manila", "bogota", "santiago", "quito", "belem", "lima"][Math.floor(Math.random() * 6)]
-  rec_otlv.value = "" + Math.floor(Math.random() * 4)
-  rec_rtmt.value = ["stochastic", "basic"][Math.floor(Math.random() * 2)]
-  rec_lomt.value = ["noise_adaptive", "trivial"][Math.floor(Math.random() * 2)]
+  try {
+
+    const resp = await fetch('https://quantum-backend-flask.herokuapp.com/recommend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // @ts-expect-error
+        code: window.translateCircuit(),
+        // @ts-expect-error
+        api_key: window.getApiKey()
+      })
+    })
+
+    const res = await resp.json()
+    if ('error' in res) throw new Error(res.error)
+
+    // check if 2 float is equal
+    const isFloatEqual = (a: number, b: number) => {
+      const diff = Math.abs(a - b)
+      return diff < 0.00001
+    }
+    // sort by least acc_error
+    const least_acc_error = res.sort((a: any, b: any) => isFloatEqual(a.acc_err, b.acc_err) ? b.optlvl - a.optlvl : a.acc_err - b.acc_err)
+    console.log(least_acc_error)
+
+    rec_sys.value = least_acc_error[0].system
+    rec_otlv.value = least_acc_error[0].optlvl + ""
+    rec_rtmt.value = least_acc_error[0].routing
+    rec_lomt.value = least_acc_error[0].layout
+
+    overlay_status.value = 'RESULT'
+  } catch (e: any) {
+    overlay_status.value = 'ERROR'
+    overlay_status_text.value = e?.message || JSON.stringify(e)
+    retry_function.value = getRecommendation
+  }
 }
 
 const useConfig = () => {
@@ -280,29 +342,32 @@ const useConfig = () => {
   otlv.value = rec_otlv.value
   rtmt.value = rec_rtmt.value
   lomt.value = rec_lomt.value
-  sdmt.value = rec_sdmt.value
 
   updateTranspileResult()
-  closeRecommendation()
+  closeOverlay()
 }
 
-const closeRecommendation = () => {
-  backend_status.value = 'IDLE'
+const closeOverlay = () => {
+  overlay_status.value = 'IDLE'
 }
 
 const toTitleCase = (str: string) => str.split('_').map((w) => w[0].toUpperCase() + w.slice(1))
   .join(' ')
 
 const initExecuteDialog = () => {
-  backend_status.value = 'FETCHING'
-  backend_status_text.value = "Connecting to Backends..."
+  overlay_status.value = 'FETCHING'
+  overlay_status_text.value = "Connecting to Backends..."
   transpile_result_status.value = 'FETCHING'
   transpile_result.value = ""
-  sys.value = "manila"
+  all_sys.value = [
+    { name: 'ibmq_qasm_simulator', qb: 32, qv: null },
+  ]
+  sys.value = "ibmq_qasm_simulator"
   otlv.value = "0"
   rtmt.value = "basic"
   lomt.value = "trivial"
   sdmt.value = "none"
+  retry_function.value = () => {}
 
   // @ts-expect-error
   qubit_count.value = window.getQubitCount()
