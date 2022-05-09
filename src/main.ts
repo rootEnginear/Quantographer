4091df3d215ecd0a52f416f51fb2bbc390d61894
rootEnginear <suthep.chanchuphol@outlook.com>
2022-05-10 01:31:22 +0700

    feat: don't allow to execute empty circuit

--- src/main.ts
+++ src/main.ts
@@ -274,6 +274,8 @@ const openExecuteDialog = async () => {
 
   // @ts-expect-error
   if (window.getApiKey() === '') return alertify.alert('No IBMQ API Key Present', 'You must enter your IBM Cloud API key before executing a circuit.')
+  // @ts-expect-error
+  if (window.isCircuitEmpty()) return alertify.alert('Circuit is Empty!', 'Your circuit is empty. Try to add some gates.')
 
   isExecuteDialogOpen = true
 
